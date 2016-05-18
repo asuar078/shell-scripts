98b7e52007f64c16b879467f3f9d65898339ad93
Arian Suarez <arian.suarez001@gmail.com>
2016-05-18 19:26:56 -0400

    Add -y to all installs

--- Setup/setup.sh
+++ Setup/setup.sh
@@ -2,32 +2,32 @@
 
 sudo apt-get update
 
-sudo apt-get install vim curl git python-software-properties
+sudo apt-get install vim curl git python-software-properties -y
 
-sudo apt-get install zsh
+sudo apt-get install zsh -y
 
 curl -L http://install.ohmyz.sh | sh
 
-sudo add-apt-repository ppa:ondrej/php5
+sudo add-apt-repository ppa:ondrej/p -y
 
-sudo apt-get update
+sudo apt-get upd -y
 
-sudo apt-get install nginx php5-fpm php5-cli php5-curl
+sudo apt-get install nginx php5-fpm php5-cli php5-c -y
 
 curl -sS https://getcomposer.org/installer | php
 
 sudo mv composer.phar /usr/bin/composer
 
-sudo add-apt-repository ppa:rquillo/ansible
+sudo add-apt-repository ppa:rquillo/ansi -y
 
 sudo apt-get update
 
-sudo apt-get install ansible
+sudo apt-get install ansible -y
 
-sudo apt-get install nfs-common nfs-kernel-server
+sudo apt-get install nfs-common nfs-kernel-server -y
 
-sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository ppa:webupd8team/java -y
 
-sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-installer -y
 
-sudo apt-get install oracle-java8-set-default
+sudo apt-get install oracle-java8-set-default -y
